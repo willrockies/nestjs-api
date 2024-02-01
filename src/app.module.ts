88bd8c45a048cae949a619c798b773414dec8457
willrockies <wlsn.ads@gmail.com>
2024-02-01 18:53:29 -0300

    instalado lib typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost'
+    })
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column({ type: 'text' })
+    description: string;
+
+    @Column()
+    image_url: string;
+
+    @Column() // decimal //
+    price: number;
+
+}
